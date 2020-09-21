b6210c219037a2648feba7c1effba59bb3c41d2e
henrikvi <hjviluksela@gmail.com>
2020-09-21 20:27:52 +0300

    style: remove unused modules

--- react-ui/src/components/Header.js
+++ react-ui/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, IconButton, Tab, Tabs } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 
 const Header = () => {
