760e391a93f1e78d14c07baccbb268bbe6b657db
henrikvi <hjviluksela@gmail.com>
2021-07-27 16:30:31 +0300

    refactor: rename client to items

--- react-ui/src/services/http/items.js
+++ react-ui/src/services/http/items.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const client = axios.create({
+const items = axios.create({
   baseURL: 'http://localhost:3001/items',
 });
 
 const getAllItems = async () => {
-  const response = await client.get();
+  const response = await items.get();
   return response.data;
 };
 
